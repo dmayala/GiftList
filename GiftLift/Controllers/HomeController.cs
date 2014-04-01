d97bdc3fc557cdf5a501865652f76458d32447f7
dmayala <dmayala@csupomona.edu>
2014-03-31 21:59:16 -0700

    Initial commit

---
+++ GiftLift/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using GiftList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiftList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var initialState = new[] {
+                new GiftModel { Title = "Tall Hat", Price = 49.95 },
+                new GiftModel { Title = "Long Cloak", Price = 78.25 }
+            };
+            return View(initialState);
+        }
+    }
+}
